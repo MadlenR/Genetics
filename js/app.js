b4050feb43a779946de6c186a2162ba489f8c845
MadlenR <rychlewska@tlen.pl>
2016-06-16 11:54:05 +0200

    Working responsive menu, not finished styling

--- js/app.js
+++ js/app.js
@@ -1,4 +1,60 @@
+////////// HEADER
+function showBtn() {
+    $('.menu_btn').removeClass('hidden_btn');
+}
 
+function hideBtn() {
+    $('.menu_btn').addClass('hidden_btn');
+}
+
+
+/////// Menu pokazywanie/ukrywanie
+function showNav() {
+    $('#nav').removeClass('hidden_nav');
+    $('#nav').find('.nav_ul').removeClass('mobile_nav');
+}
+
+function hideNav() {
+    $('#nav').addClass('hidden_nav');
+    $('#nav').find('.nav_ul').addClass('mobile_nav');
+}
+
+////////// Zawartość hamburger menu
+function btnMenuInit() {
+    var $nav = $('#nav');
+    
+    $('.btn-menu').on('click', function(event){
+        event.preventDefault();
+        $nav.toggleClass('hidden_nav');
+    });
+}
+
+
+function test_match_media_with_listener() {
+    var mq = window.matchMedia("(max-width: 500px)");
+    console.log(mq);
+    
+    mq.addListener(WidthChange);
+    WidthChange(mq);
+
+    
+    function WidthChange(mediaQuery) {
+        if(mediaQuery.matches) {
+            console.log("yes");
+            hideNav();
+            showBtn();
+
+        } else {
+            console.log("no");
+            showNav();
+            hideBtn();
+        }
+    }
+}
+
+$(document).ready(function() {
+  
+});
 
 
 ////////// SECTION Q&A
@@ -18,59 +74,62 @@ function clickQA (element) {
 }
 
 
+//
+////////////// SECTION FORM
+//
+//function handleForm () {
+//
+//var form= $('form');
+//var nameInput = form.find('#nameInput');
+//var emailInput = form.find('#emailInput');
+//var messageInput= form.find('messageInput');
+//var errordiv= form.find('error');
+//
+//
+//form.on('submit', function (ev) {
+//
+//    var name =nameInput.val();
+//    var email= emailInput.val();
+//    var message= messageInput.val();
+//    
+//    var nameok = name.length > 3; 
+//    var emailok = email.indexOf('@') !=-1;
+//    var messageok= message.length >10;
+//    
+//    
+//    errordiv.html('');
+//    
+//    
+//    function showError(err) {
+//        errordiv.html(errordiv.html() + err + '<br>');
+//        console.log(errordiv.html());
+//    }
+//    
+//    
+//    if(!nameok || !emailok || !messageok) {
+//        
+//        
+//        if (!nameok) showError('Your name is too short.');
+//        if (!emailok) showError('Your mail is invalid. Please enter proper e-mail address including "@" sign.');
+//        if (!messageok) showError('Your message is too short.');
+//   
+//         ev.preventDefault();
+//    
+//    } 
+//   
+//    
+//}); 
+//
+//}
 
-//////////// SECTION FORM
-
-function handleForm () {
-
-var form= $('form');
-var nameInput = form.find('#nameInput');
-var emailInput = form.find('#emailInput');
-var messageInput= form.find('messageInput');
-var errordiv= form.find('error');
-
-
-form.on('submit', function (ev) {
-
-    var name =nameInput.val();
-    var email= emailInput.val();
-    var message= messageInput.val();
-    
-    var nameok = name.length > 3; 
-    var emailok = email.indexOf('@') !=-1;
-    var messageok= message.length >10;
-    
-    
-    errordiv.html('');
-    
-    
-    function showError(err) {
-        errordiv.html(errordiv.html() + err + '<br>');
-        console.log(errordiv.html());
-    }
-    
-    
-    if(!nameok || !emailok || !messageok) {
-        
-        
-        if (!nameok) showError('Your name is too short.');
-        if (!emailok) showError('Your mail is invalid. Please enter proper e-mail address including "@" sign.');
-        if (!messageok) showError('Your message is too short.');
-   
-         ev.preventDefault();
-    
-    } 
-   
-    
-}); 
-
-}
-
-
+/////////////////////
+///////////DOCUMENT READY////////////////////////////////
+////////////////////
 
 $(document).ready(function(){
 //        console.log("działa");
     
     clickQA($(".QA"));
-    
+    test_match_media_with_listener();
+    btnMenuInit();
 });
